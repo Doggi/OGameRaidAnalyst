81b6596c52a080510cb01c86d77a81ad3f542abd
Sören Grzanna <soeren.grzanna@stud.fh-hannover.de>
2016-01-04 13:40:44 +0100

    Initial commit

---
+++ OGame_Raid_Analyst.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name        OGame Raid Analyst
+// @namespace   ogame
+// @include     http://*.ogame.gameforge.com/game/index.php?page=overview*
+// @version     1
+// @grant       none
+// ==/UserScript==
